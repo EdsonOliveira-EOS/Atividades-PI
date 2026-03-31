1c92090814da8ef07468424c5cec553c60590aea
Phaidros <oliveiraedson.silva2007@hotmail.com>
2026-03-31 13:08:15 -0300

    Questão 1 da lista 4

---
+++ Lista-PI/Lista-4/Questão1.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int printmatrix (int m, int n, char matrix[m][n]){
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            printf ("%c", matrix[i][j]);
+        }
+        printf ("\n");
+    }
+
+    return 0;
+}
+
+int main(void){
+
+    int m, n;
+    scanf("%d %d\n", &m, &n);
+    // m = linhas
+    // n = colunas
+
+    char matrix[m][n];
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            char temp;
+            scanf("%c", &temp);
+            matrix[i][j] = temp;
+        }
+    
+        char temp2; scanf("%c", &temp2);
+    }
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            if (matrix[i][j] == 'P') {
+                if (i > 0 && (matrix[i-1][j] == '-' || matrix[i-1][j] == 'x')) {
+                    matrix[i-1][j] = '+';
+                }
+                if (i < m-1 && (matrix[i+1][j] == '-' || matrix[i+1][j] == 'x')) {
+                    matrix[i+1][j] = '+';
+                }
+                if (j > 0 && (matrix[i][j-1] == '-' || matrix[i][j-1] == 'x')) {
+                    matrix[i][j-1] = '+';
+                }
+                if (j < n-1 && (matrix[i][j+1] == '-' || matrix[i][j+1] == 'x')) {
+                    matrix[i][j+1] = '+';
+                }
+
+                if (i > 0 && j > 0 && (matrix[i-1][j-1] == '-' || matrix[i-1][j-1] == 'x')) {
+                    matrix[i-1][j-1] = 'x';
+                }
+                if (i > 0 && j < n-1 && (matrix[i-1][j+1] == '-' || matrix[i-1][j+1] == 'x')) {
+                    matrix[i-1][j+1] = 'x';
+                }
+                if (i < m-1 && j > 0 && (matrix[i+1][j-1] == '-' || matrix[i+1][j-1] == 'x')) {
+                    matrix[i+1][j-1] = 'x';
+                }
+                if (i < m-1 && j < n-1 && (matrix[i+1][j+1] == '-' || matrix[i+1][j+1] == 'x')) {
+                    matrix[i+1][j+1] = 'x';
+                }
+            }
+        }        
+    }
+
+    printmatrix(m, n, matrix);
+
+    return 0;
+}
