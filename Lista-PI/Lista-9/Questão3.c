8506e3840c98ca8e37b9368b7cc7eaae1af7a1b4
Phaidros <oliveiraedson.silva2007@hotmail.com>
2026-06-01 01:56:50 -0300

    Ajeitando a questão 3

--- Lista-PI/Lista-9/Questão3.c
+++ Lista-PI/Lista-9/Questão3.c
@@ -12,55 +12,52 @@ void checknull (void *pointer){
 typedef struct Tarefa {
     int id;
     int prioridade;
-    struct Tarefa *nextpointer;
+    struct Tarefa *prox; 
 } Tarefa;
 
 void inserir (int id, int prioridade, Tarefa **startpointer, Tarefa **endpointer){
 
-    // Alocar dinamicamente
     Tarefa *nova = malloc(sizeof(Tarefa));
     checknull(nova);
 
-    // Colocar os dados
     nova->id = id;
     nova->prioridade = prioridade;
-    nova->nextpointer = NULL;
+    nova->prox = NULL;
 
-    // Se startpointer == NULL, quer dizer que a lista está vazia.
     if (*startpointer == NULL){ 
         (*startpointer) = nova;
         (*endpointer) = nova;
     } else { 
-        // Se não tá vazia, o ponteiro do fim vai apontar para o próximo da lista e o ponteiro do final vai apontar para NULL
-        (*endpointer)->nextpointer = nova;
+        (*endpointer)->prox = nova;
         (*endpointer) = nova;
     }
 }       
 
 void remover (Tarefa **startpointer, Tarefa **endpointer){
-    if ((*startpointer) == NULL){ // Se a lista estiver vazia
+    if ((*startpointer) == NULL){ 
         printf ("Fila vazia\n");
         return;
     }
     
-    Tarefa (*temp) = (*startpointer);
+    Tarefa *temp = (*startpointer);
     printf ("Removida: id=%d prioridade=%d\n", temp->id, temp->prioridade);
     
-    (*startpointer) = (*startpointer)->nextpointer;
+    (*startpointer) = (*startpointer)->prox;
 
     if ((*startpointer) == NULL){
         (*endpointer) = NULL;
     }
     free (temp);
 }
+
 void listar (Tarefa *startpointer){
-    if (startpointer == NULL){ // Se a lista estiver vazia
+    if (startpointer == NULL){ 
         printf ("Fila vazia\n");
     } else {
         Tarefa *atual = startpointer;
         while (atual != NULL){
             printf ("id=%d prioridade=%d\n", atual->id, atual->prioridade);
-            atual = atual->nextpointer;
+            atual = atual->prox;
         }
     }
 }
@@ -85,5 +82,11 @@ int main(){
                 break;
         }
     }
+    while (startpointer != NULL) {
+        Tarefa *temp = startpointer;
+        startpointer = startpointer->prox;
+        free(temp);
+    }
+
     return 0;
 }
